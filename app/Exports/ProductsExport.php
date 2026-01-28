8a5df3dc5d504f6cc572823882886c614d6238bd
Prokushka <anton.pronin.05@list.ru>
2026-01-28 12:32:17 +0300

    forgot about excel)

---
+++ app/Exports/ProductsExport.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Product;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+
+class ProductsExport implements FromCollection, WithStyles, WithMapping, WithHeadings, ShouldAutoSize, WithEvents
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Product::with('priceTiers')->get();
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => [
+                'font' => ['bold' => true],
+                'alignment' => [
+                    'horizontal' => 'center',
+                ],
+            ],
+        ];
+    }
+    public function headings(): array
+    {
+        return [
+            'Название',
+            'Цена (от)',
+            'Количество',
+            'Объём',
+        ];
+    }
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+
+                $sheet = $event->sheet->getDelegate();
+
+                $headerRange = 'A1:D1';
+
+                $sheet->getStyle($headerRange)->applyFromArray([
+                    'font' => [
+                        'bold' => true,
+                        'color' => ['rgb' => 'FFFFFF'], // белый текст
+                    ],
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'startColor' => [
+                            'rgb' => '2E7D32', // зелёный
+                        ],
+                    ],
+                    'alignment' => [
+                        'horizontal' => Alignment::HORIZONTAL_CENTER,
+                        'vertical' => Alignment::VERTICAL_CENTER,
+                    ],
+                ]);
+
+                $sheet->freezePane('A2');
+                // Фильтры
+                $sheet->setAutoFilter($headerRange);
+            },
+        ];
+    }
+
+    public function map($row): array
+    {
+        $opt = optional($row->priceTiers->latest())->price;
+        return [
+            $row->title,
+            "$opt руб.",
+            $row->quantity,
+            $row->size,
+        ];
+    }
+}
